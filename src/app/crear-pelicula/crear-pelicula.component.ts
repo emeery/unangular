499f5f333b009a79e0dad6fdc8798d5b07d4ca36
emeery <gerardoemery@live.com.mx>
2018-08-22 22:52:41 -0500

    parametro

--- src/app/crear-pelicula/crear-pelicula.component.ts
+++ src/app/crear-pelicula/crear-pelicula.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute } from '@angular/router';
-import {Pelicula} from '../model/pelicula'; // modelo
-import {PeliculasServicio} from '../services/peliculas.services'; /*servicios o funciones*/
+import {Pelicula} from '../model/pelicula';
+import {PeliculasServicio} from '../services/peliculas.services';
+import {Router, ActivatedRoute} from '@angular/router';
+
 @Component({
   selector: 'app-crear-pelicula',
   templateUrl: './crear-pelicula.component.html',
   styleUrls: ['./crear-pelicula.component.css'],
   providers: [PeliculasServicio],
 })
-/*public id: number;*/
 export class CrearPeliculaComponent implements OnInit {
-  public untitulo = '';
+	public tituloPelicula = "";
+  public directorPelicula = "";
+  public fecha;
   public nuevaPelicula: Pelicula;
-  constructor(
-    private _unServicio: PeliculasServicio,
-    private _router: Router,
-    private route: ActivatedRoute) {}
-  unSubmit() {
+	constructor(
+	private _unServicio: PeliculasServicio,
+	private _unRouter: Router,
+  private route: ActivatedRoute )
+	{}
+  unSubmit () {
     this._unServicio.insertarPelicula(this.nuevaPelicula);
-    this._router.navigate(['/peliculas']);
+    this._unRouter.navigate(['/']);
   }
-  ngOnInit() {
-    // Recogemos los parametros de la URL
-     this.route.params.subscribe(params => {
-       if (params['titulo'] != null) {
-        this.untitulo = params['titulo'];
-        }
-     });
-}
+  ngOnInit():any {
+        // Recogemos los parametros de la URL
+         this.route.params.subscribe(params => {
+            if(params['titulo']!=null){
+                this.tituloPelicula = params['titulo'];
+            }
+            this.directorPelicula = params['director'];
+            this.fecha = params['fecha'];
+         });
+         this.nuevaPelicula = new Pelicula(
+             0,
+             this.tituloPelicula,
+             this.directorPelicula,
+             this.fecha
+           );
+    }
 }
